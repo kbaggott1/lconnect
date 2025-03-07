26e4adb9df48fe1ddf63f28dd6c4172e45b4c679
kbaggott1 <kevin.baggott1@gmail.com>
2025-03-06 20:03:02 -0500

    init mobile app

---
+++ mobile/Lconnect/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lconnect
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ mobile/Lconnect/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Lconnect
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
